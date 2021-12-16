8ccaf8275c21b926e18fbc31591523555b39d4c0
Vist <vist-amur@yandex.ru>
2021-12-16 11:00:23 +0900

    Обновление 16.12.2021

---
+++ venvRGZ/view_report_gz.py
@@ -0,0 +1,19 @@
+import datetime
+import streamlit as st
+
+def load_css(file_name:str):
+    '''
+        Function to load and render a local stylesheet
+    '''
+    with open(file_name) as f:
+        d = f.read()
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+st.sidebar.text('Отборы')
+selected_regions = st.sidebar.multiselect("Регионы РФ", ['Амурская область', 'Хабаровский край'])
+date_start = st.sidebar.date_input('Период размещения заявок:',[datetime.datetime.now() - datetime.timedelta(1), datetime.datetime.now()])
+like_words = st.sidebar.text_input('Ключевые фразы, указанные через запятую:')
+
+load_css(r'C:\Users\DNS\report_goszakupki\venvRGZ\style.css')
+#comment а
