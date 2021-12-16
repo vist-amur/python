56f820879cf2a640627779444e00d85ed2cb2302
Vist <vist-amur@yandex.ru>
2021-12-16 11:34:07 +0900

    Обновление 16.12.2021

--- venvRGZ/view_report_gz.py
+++ venvRGZ/view_report_gz.py
@@ -1,5 +1,6 @@
 import datetime
 import streamlit as st
+from PIL import Image
 
 def load_css(file_name:str):
     '''
@@ -10,7 +11,16 @@ def load_css(file_name:str):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-st.sidebar.text('Отборы')
+image_directory = r"C:\Users\DNS\report_goszakupki\venvRGZ\logo.PNG"
+image = Image.open(image_directory)
+
+PAGE_CONFIG = {"page_title":"База данных извещений с сайта zakupki.gov.ru",
+               "page_icon":image,
+               "layout":"centered",
+               "initial_sidebar_state":"auto"}
+
+st.set_page_config(**PAGE_CONFIG)
+
 selected_regions = st.sidebar.multiselect("Регионы РФ", ['Амурская область', 'Хабаровский край'])
 date_start = st.sidebar.date_input('Период размещения заявок:',[datetime.datetime.now() - datetime.timedelta(1), datetime.datetime.now()])
 like_words = st.sidebar.text_input('Ключевые фразы, указанные через запятую:')
