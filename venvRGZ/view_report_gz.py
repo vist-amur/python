2068ea2ab1cae7220f2cabe9b070ed7127cc20ee
Vist <vist-amur@yandex.ru>
2021-12-18 21:40:46 +0900

    Обновление 18.12.2021

--- venvRGZ/view_report_gz.py
+++ venvRGZ/view_report_gz.py
@@ -1,16 +1,18 @@
+import base64
 import datetime
 import streamlit as st
 from PIL import Image
 import pymysql
 import platform
 import sys
-import plotly.express  as px
+import plotly.express as px
 import plotly.graph_objects as go
 from altair.vega import alignValue
 import pandas as pd
+import csv
 
 
-def load_css(file_name:str):
+def load_css(file_name: str):
     '''
         Function to load and render a local stylesheet
     '''
@@ -18,13 +20,16 @@ def load_css(file_name:str):
         d = f.read()
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
 def decryption_of_parameters():
     print('Разбор параметров '
-           '1.Адрес БД; 2.Пользователь БД; 3.Пароль пользователя БД; 4.Имя БД; 5.Имя таблицы извещений)')
-    print(r'Пример: 94.228.121.182 phpmyadmin pass goszakupki notifications C:\Users\DNS\report_goszakupki\venvRGZ '
-          r'logo.png C:\Users\DNS\report_goszakupki\venvRGZ style.css')
+          '1.Адрес БД; 2.Пользователь БД; 3.Пароль пользователя БД; 4.Имя БД; 5.Имя таблицы извещений)')
+    print(r'Пример: 94.228.121.182 phpmyadmin pass goszakupki notifications '
+          r'C:\Users\User\PycharmProjects\pythonProject\venvRGZ logo.png '
+          r'C:\Users\User\PycharmProjects\pythonProject\venvRGZ style.css')
 
-def get_region(p_key, p_status = False):
+
+def get_region(p_key, p_status=False):
     dict_regions = {'Altaj_Resp': 'Республика Алтай',
                     'Altajskij_kraj': 'Алтайский край',
                     'Amurskaja_obl': 'Амурская область',
@@ -117,25 +122,41 @@ def get_region(p_key, p_status = False):
             return key
     return ""
 
-def tuple_to_Dataframe(p_tuple):
+
+def tuple_to_dataframe(p_tuple):
     p_list = []
     for x in p_tuple:
         a = [y for y in x]
         p_list.append(a)
     return p_list
 
+
+def to_csv(p_tuple):
+    with open('tuple.csv', 'w') as f:
+        write = csv.writer(f)
+        write.writerows(p_tuple)
+
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    return href
+
 def main():
     p_address = sys.argv[1]
-    p_loginDB = sys.argv[2]
-    p_passDB = sys.argv[3]
-    p_nameDB = sys.argv[4]
+    p_login_db = sys.argv[2]
+    p_pass_db = sys.argv[3]
+    p_name_db = sys.argv[4]
     p_nametable = sys.argv[5]
     p_logo = sys.argv[6]
     p_name_logo = sys.argv[7]
     p_css = sys.argv[8]
     p_name_css = sys.argv[9]
-
-
 
     p_slash = ''
     p_os = platform.system().lower()
@@ -150,24 +171,24 @@ def main():
     finally:
         image = Image.new(mode="RGB", size=(120, 120))
 
-    PAGE_CONFIG = {"page_title":"База данных извещений с сайта zakupki.gov.ru",
-                   "page_icon":image,
-                   "layout":"centered",
-                   "initial_sidebar_state":"auto"}
+    PAGE_CONFIG = {"page_title": "База данных извещений с сайта zakupki.gov.ru",
+                   "page_icon": image,
+                   "layout": "centered",
+                   "initial_sidebar_state": "auto"}
 
     st.set_page_config(**PAGE_CONFIG)
-    table_contener = st.container()
     table_contener_df = st.container()
-    connection = pymysql.connect(host=p_address, user=p_loginDB, passwd=p_passDB, database=p_nameDB)
+    connection = pymysql.connect(host=p_address, user=p_login_db, passwd=p_pass_db, database=p_name_db)
 
     st.sidebar.markdown('## Фильтры')
-    selected_regions = st.sidebar.multiselect("Регионы РФ", get_region('',True))
-    date_start = st.sidebar.date_input('Период размещения заявок:',[datetime.datetime.now() - datetime.timedelta(1), datetime.datetime.now()])
+    selected_regions = st.sidebar.multiselect("Регионы РФ", get_region('', True))
+    date_start = st.sidebar.date_input('Период размещения заявок:',
+                                       [datetime.datetime.now() - datetime.timedelta(1), datetime.datetime.now()])
     like_words = st.sidebar.text_input('Ключевые фразы объекта закупки, указанные через запятую:')
     list_like_words = like_words.split(',')
-    #with st.echo():
+    # with st.echo():
     #    st.write(selected_regions)
-    #with st.echo():
+    # with st.echo():
     #    st.write(list_like_words)
     sql_like_words = ''
     ind = 0
@@ -183,9 +204,6 @@ def main():
         else:
             sql_like_words = ''
 
-
-    sql_regions = []
-    p_sql_regions = ""
     p_sql_regions_pre = " AND region IN ("
     if len(selected_regions) > 0:
         sql_regions = [f"\'{get_region(x)}\'" for x in selected_regions if get_region(x) != "" f"'{get_region(x)}'"]
@@ -193,26 +211,27 @@ def main():
     else:
         p_sql_regions = ""
         p_sql_regions_pre = ""
-        #sql_regions = sql_regions + f' AND region IN ({sql_regions})'
-    #SELECT * FROM `notifications` WHERE `date_publish` BETWEEN '2021-12-13' AND '2021-12-13'
-    #AND `region` IN () AND `subject_purchase`LIKE ''
+        # sql_regions = sql_regions + f' AND region IN ({sql_regions})'
+    # SELECT * FROM `notifications` WHERE `date_publish` BETWEEN '2021-12-13' AND '2021-12-13'
+    # AND `region` IN () AND `subject_purchase`LIKE ''
     cursor = connection.cursor()
     sql = f"Select date_publish,date_finish,fz,type_fz,region,num_notification,price,subject_purchase,customer, address_customer, ref from {p_nametable} where date_publish BETWEEN '{date_start[0]}' AND '{date_start[-1]}'{p_sql_regions_pre}{p_sql_regions}{sql_like_words}" \
           f" ORDER BY date_publish, region"
     cursor.execute(sql)
-    #with st.echo():
+    # with st.echo():
     #    st.write(sql)
 
     AllRow = cursor.fetchall()
-    #st.dataframe()
-    #st.table(AllRow)
+    # st.dataframe()
+    # st.table(AllRow)
 
-    #list_a = [['1', '2', '3'],
+    # list_a = [['1', '2', '3'],
     #          ['6', '7', '8']]
-    columns = ['Дата_публикации','Дата_окончания', 'ФЗ', 'Тип_закупки', 'Регион', 'Номер_извещения', 'Начальная_цена', 'Предмет_закупки', 'Заказчик', 'Адрес_заказчика', 'Ссылка']
-    df_a = pd.DataFrame(tuple_to_Dataframe(AllRow), columns=columns)
+    columns = ['Дата_публикации', 'Дата_окончания', 'ФЗ', 'Тип_закупки', 'Регион', 'Номер_извещения', 'Начальная_цена',
+               'Предмет_закупки', 'Заказчик', 'Адрес_заказчика', 'Ссылка']
+    df_a = pd.DataFrame(tuple_to_dataframe(AllRow), columns=columns)
 
-    #with table_contener:
+    # with table_contener:
     #    fig = go.Figure(data=go.Table(
     #                    header=dict(values=['Дата публикации','Дата окончания', 'ФЗ', 'Тип закупки', 'Регион', 'Номер извещения', 'Начальная цена', 'Предмет закупки', 'Заказчик', 'Адрес заказчика', 'Ссылка'], fill_color='#dde23e', align='center'),
     #                    cells=dict(values=[df_a.Дата_публикации,df_a.Дата_окончания,df_a.ФЗ, df_a.Тип_закупки, df_a.Регион, df_a.Номер_извещения, df_a.Начальная_цена, df_a.Предмет_закупки, df_a.Заказчик, df_a.Адрес_заказчика, df_a.Ссылка],font = dict(color = 'darkslategray', size = 12))))
@@ -221,10 +240,14 @@ def main():
     #    st.write(fig)
 
     with table_contener_df:
-       st.dataframe(df_a)
-
-    #load_css(fr'{p_css}{p_slash}{p_name_css}')
-
+        st.dataframe(df_a)
+    if st.button('Сохранить таблицу в csv файл'):
+        p_xls = df_a.to_excel("data_goszakupki.xlsx")
+        st.markdown(f'<a href="http://localhost:8501/data_goszakupki.xlsx">Download csv file</a>', unsafe_allow_html=True )
+    # load_css(fr'{p_css}{p_slash}{p_name_css}')
+    #st.download_button('Download CSV', text_contents, 'text/csv')
+    #with open('myfile.csv') as f:
+    #    st.download_button('Download CSV', f)  # Defaults to 'text/plain'
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
